550b557dc8f193d4b103013944c7f32f08e309c9
Zwornik <kruszelnickim@gmail.com>
2022-12-08 01:35:42 +0000

    Struggle with information about whose move that was

--- TTT-1.py
+++ TTT-1.py
@@ -2,22 +2,50 @@
 
 from random import choice
 
-state = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-print(type(state))
+state = [" ", " ", " ", " ", " ", " ", " ", " ", " "]  # Keeps current game state
 
 
-def display(state):
+def display(state, first_move):  # Display game result
+    whi = who(state, first_move)
+
+    if whi != "":
+        whi = "◀◀◀ {0}".format(whi)
+
     print("        A   B   C  ".format(state))
     print("      ╔═══╤═══╤═══╗")
     print("    1 ║ {0[0]} │ {0[3]} │ {0[6]} ║".format(state))
     print("      ╟───┼───┼───╢")
-    print("    2 ║ {0[1]} │ {0[4]} │ {0[7]} ║".format(state))
+    print("    2 ║ {0[1]} │ {0[4]} │ {0[7]} ║   {1}".format(state, whi))
     print("      ╟───┼───┼───╢")
     print("    3 ║ {0[2]} │ {0[5]} │ {0[8]} ║".format(state))
     print("      ╚═══╧═══╧═══╝\n")
 
 
-def check(state):
+def who(state, first_move):  # find if move was made by X or O
+    global temp_state
+    sign = ""
+    if first_move == 0:  # assign state to temp_state but only first time
+        temp_state = state
+        print("first")
+
+    for i in range(len(state)):  # find out whose move was recent
+        if len(state[i]) > len(temp_state[i]):
+            sign = state[i]
+            print(temp_state, "< temp", first_move)
+            print(state, "< state")
+            temp_state = state
+        print(sign, ":::")
+        return sign
+
+    return sign
+
+    # znajdź różnicę między temp_state i state
+
+
+
+
+
+def check(state):  # Check if there is a win (row of 3)
     if state[0] == state[1] == state[2] or \
             state[3] == state[4] == state[5] in ("X", "O") or \
             state[6] == state[7] == state[8] in ("X", "O") or \
@@ -30,7 +58,7 @@ def check(state):
         print(state)
 
 
-def user_inp():
+def user_inp():  # collect user move
     inp = ""
     inp = input("Type field coordinates to put the 'X' (e.g. 'A2')")
     inp = inp.upper()
@@ -38,7 +66,7 @@ def user_inp():
         print("WRONG INPUT. TRY AGAIN.")
         user_inp()
     elif read(state, inp) != " ":
-        print("This filed has '{}'".format(read(state, inp)))
+        print("There is '{}' in this field. Please try again.".format(read(state, inp)))
     else:
         print("ok")
         return inp
@@ -81,6 +109,7 @@ def empty_corner(state):
         empty_corners.append(6)
     elif state[8] == " ":
         empty_corners.append(8)
+    print(empty_corners)
     return empty_corners
 
 
@@ -92,13 +121,13 @@ def response(state):
 
 
 print("\n_________________________\nThis is TIC TOC TOE game.\n_________________________\n")
-display(state)
+display(state, 0)
 
 while True:
     inp = user_inp()
     insert(state, "X", inp)
-    display(state)
+    display(state, 1)
     response(state)
 
-    display(state)
+    display(state, 1)
     check(state)
