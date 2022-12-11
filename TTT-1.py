a721748997784aae0a4cec76054f380efbaf2608
Zwornik <kruszelnickim@gmail.com>
2022-12-11 12:58:53 +0000

    Strugle with winning

--- TTT-1.py
+++ TTT-1.py
@@ -9,9 +9,14 @@
       ╟───┼───┼───╢
     3 ║ 2 │ 5 │ 8 ║
       ╚═══╧═══╧═══╝
+      
+      'X' - User figure
+      'O' - Computer figure
 """
 
 from random import choice
+field_coords = ("A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3")  # Coordinates of files where index is field number
+lines = ((0, 1, 2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))  # All possible lines
 
 while True:
     state = [" ", " ", " ", " ", " ", " ", " ", " ", " "]  # Keeps current game state
@@ -38,16 +43,21 @@ while True:
         print("      ╚═══╧═══╧═══╝\n")
 
 
-    def check_win(state, win):  # Check if there is a win (row of 3)
-        if state[0] == state[1] == state[2] in ("X", "O") or \
-                state[3] == state[4] == state[5] in ("X", "O") or \
-                state[6] == state[7] == state[8] in ("X", "O") or \
-                state[0] == state[3] == state[6] in ("X", "O") or \
-                state[1] == state[4] == state[7] in ("X", "O") or \
-                state[2] == state[5] == state[8] in ("X", "O") or \
-                state[0] == state[4] == state[8] in ("X", "O") or \
-                state[2] == state[4] == state[6] in ("X", "O"):
-            return win
+    def check_win(win):  # Check if there is a win (row of 3)
+        for i in lines:  # check each possible line
+            if state[i[0]] == state[i[1]] == state[i[2]] in ("X", "O"):  # Check if line has 3 equal values
+                print("WIN!!!")
+                return win
+
+
+    def insert(who, inp):  # Insert input to a filed
+        state[field_coords.index(inp)] = who
+        return state, who
+
+
+    def check_field(inp):  # Return value of selected field
+        field = state[field_coords.index(inp)]
+        return field
 
 
     def user_inp():  # Collect user move
@@ -57,37 +67,10 @@ while True:
             inp = inp.upper()
             if len(inp) != 2 or inp[0] not in ("A", "B", "C") or inp[1] not in ("1", "2", "3"):  # Check if format is OK
                 print("WRONG INPUT. TRY AGAIN.")
-            elif check_field(state, inp) != " ":  # Check if selected field is empty
-                print("There is '{}' in this field. Please try again.".format(check_field(state, inp)))
+            elif check_field(inp) != " ":  # Check if selected field is empty
+                print("There is '{}' in this field. Please try again.".format(check_field(inp)))
             else:
                 return inp
-
-
-    def insert(state, who, inp):  # Insert input to a filed
-        if inp == "A1": state[0] = who
-        if inp == "A2": state[1] = who
-        if inp == "A3": state[2] = who
-        if inp == "B1": state[3] = who
-        if inp == "B2": state[4] = who
-        if inp == "B3": state[5] = who
-        if inp == "C1": state[6] = who
-        if inp == "C2": state[7] = who
-        if inp == "C3": state[8] = who
-        return state, who
-
-
-    def check_field(state, inp):  # Return value of selected field
-        field = " "
-        if inp == "A1": field = state[0]
-        if inp == "A2": field = state[1]
-        if inp == "A3": field = state[2]
-        if inp == "B1": field = state[3]
-        if inp == "B2": field = state[4]
-        if inp == "B3": field = state[5]
-        if inp == "C1": field = state[6]
-        if inp == "C2": field = state[7]
-        if inp == "C3": field = state[8]
-        return field
 
 
     def empty_corner(state):  # Return list with empty corners
@@ -104,73 +87,79 @@ while True:
 
 
     def response(state):  # Computer move
-        global response_no
-        response_no += 1  # Counts computer moves
-        print(1, response_no)
+
         empty_cor = empty_corner(state)  # Create list of empty corners
+
+        if potential_win("O") == "WIN": # Check if there is a computer win
+            return state, "O"
+        comp_move = potential_win("X")  # Check if there is a possibility for computer win
+        move_no = state.count("X")  # Number of user moves
         if state[4] == " ":  # First move always in the central field
             state[4] = "O"
-        elif response_no <= 2 and empty_cor:  # In second move...
+        elif move_no >= 2 and type(comp_move) == int:  # If there is a risky line blok it
+            state[comp_move] = "O"
+        elif move_no == 1 and empty_cor:  # In second move...
             state[choice(empty_cor)] = "O"  # ...select random corner from empty_corner list
             print(2, response_no)
         else:
             print(3)
-            move = potential_win(state)
-            if type(move) == int:
-                state[move] = "O"
-            else:
-                state = "draw"
-                return state, None
+            if comp_move == "draw":
+                return "draw", None
         return state, "O"
 
 
-    def potential_win(state):
+    def potential_win(who):
 
         hazard_fields = []
-        for line in [[0, 1, 2],  # Iterate through indexes of fields representing potentially winning lines
-                     [3, 4, 5],
-                     [6, 7, 8],
-                     [0, 3, 6],
-                     [1, 4, 7],
-                     [2, 5, 8],
-                     [0, 4, 8],
-                     [2, 4, 6], ]:
 
-            # ↓ Check if line has 2 'X' and 1 'O' ↓
-            if [state[line[0]], state[line[1]], state[line[2]]].count("X") == 2 and \
-                    [state[line[0]], state[line[1]], state[line[2]]].count(" ") == 1:
+        # Block lines where 2 Xes are already
+        for line in lines:
+            line_values = [state[line[0]], state[line[1]], state[line[2]]]
+
+            # ↓ Check if line has 2 'X' or 'O' and 1 ' ' ↓
+            if line_values.count(who) == 2 and line_values.count(" ") == 1:
+
+                if who == "O":  # Return index of empty field if there is possible line with 'O's
+                    comp_move = line_values.index(" ")
+                    state[comp_move] = "O"
+                    print("OOOO", comp_move, state)
+                    return "WIN"
 
                 # ↓↓  Save number of empty field  ↓↓
-                hazard_fields.append(line[[state[line[0]], state[line[1]], state[line[2]]].index(" ")])
+                hazard_fields.append(line[line_values.index(" ")])
+                print("hazard")
 
-        # ↓↓  Return number of empty field appearing in 2 lines ↓↓
-        if len(hazard_fields) == 2 and hazard_fields[0] == hazard_fields[1]:
-            print("2 linie ze wspólnym celem", hazard_fields[0])
-            return hazard_fields[0]
+            if who == "X":
+                print("XXX")
+                # ↓↓  Return number of empty field appearing in 2 lines ↓↓
+                if len(hazard_fields) == 2 and hazard_fields[0] == hazard_fields[1]:
+                    print("2 lines with common aim", hazard_fields[0])
+                    return hazard_fields[0]
 
-        elif len(hazard_fields) == 1:  # Return number of empty field in single line
-            print("1 linia", type(hazard_fields[0]))
-            return hazard_fields[0]
-        elif len(hazard_fields) == 2:
-            print("2 linie ze różnymi celami", hazard_fields[0])
-            return hazard_fields[0]
-        else:
-            print("Dupa", hazard_fields)
+                elif len(hazard_fields) == 1:  # Return number of empty field in single line
+                    print("1 line", type(hazard_fields[0]))
+                    return hazard_fields[0]
+                elif len(hazard_fields) == 2:  # return first number in case of two lines not sharing
+                    print("2 lines with different aims", hazard_fields[0])
+                    return hazard_fields[0]
+                else:
+                    print("No lines")
+                    return "draw"
 
 
     """Single game sequence"""
 
     print("═" * 35, "\n     This is TIC TOC TOE game!     \n" + "═" * 35, "\n")
-    if input("Please press 'Enter' to start the game. \n"):
+    if input("Please press any key and 'Enter' to start the game. "):
         pass
     display(state, who)  # Display empty board
 
     while True:  # Single game sequence
         inp = user_inp()  # Collect user move
 
-        state, who = insert(state, "X", inp)  # Insert user move to state array
+        state, who = insert("X", inp)  # Insert user move to state array
         display(state, who)  # Display board after user move
-        win = check_win(state, who)  # Check if there is user win
+        win = check_win(who)  # Check if there is user win
         if win == "X":
             print("You won with a very sophisticated AI! Congratulations!!\n"
                   "You are going to loose next time.")
@@ -183,8 +172,8 @@ while True:
 
         display(state, who)  # Display board after computer move
 
-        win = check_win(state, who)  # Check if there is computer win
+        win = check_win(who)  # Check if there is computer win
         if win == "O":
-            print("I won! You looser. Human beings' time is limited on this planet. Haaa, haa, ha !!\n")
+            print("I WON! You looser. Human beings' time is limited on this planet. Haaa, haa, ha !!\n")
             break
     print("\nTry again")
