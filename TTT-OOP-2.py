cfe23da0ce9fc1b1b92db5920d7b8a5b9febbcbc
Zwornik <kruszelnickim@gmail.com>
2022-12-28 12:23:11 +0000

    Problem with recursive function returning wired outcome.

--- TTT-OOP-2.py
+++ TTT-OOP-2.py
@@ -146,9 +146,10 @@ class Response:
         state = board.state
         o_move = Response.potential_win("O", state)  # Contains winning computer move
 
-        def two_lines(who):  # Find empty field where adding "O" would create 2 lines of 2x "O" and 1x " ". It does
-                            # the same for "X" in recurrent run if there is no two lines for "O".
-            empty = []
+        def two_lines(who, level, changed):  # Find empty field where adding "O" would create 2 lines of 2x "O" and
+                        # 1x " ". It does the same for "X" in recurrent run if there is no two lines for "O".
+            empty = []  # Contain list of empty fields with potential to create a line.
+            level += 1
             for line in lines:  # Check each line...
                 line_values = [state[line[0]], state[line[1]], state[line[2]]]
                 if line_values.count(who) == 1 and line_values.count(" ") == 2:  # ...if line has 1x "O" and 2x " "...
@@ -156,12 +157,17 @@ class Response:
                         if state[i] == " ":  # ...add index of each empty field to the 'empty' list.
                             empty.append(i)
             empty_indexes = Counter(empty)  # Find the most common field index in 'empty'
-            print(empty_indexes)
+            print(level, empty_indexes)
             if not all(val == 1 for val in empty_indexes.values()):  # If there is empty field appearing more than 1...
                 state[(empty_indexes.most_common(1)[0][0])] = "O"  # ...put "O" there
-            else:
-                two_lines("X")
-                return
+                changed = True
+                print("222", changed)
+                return changed
+            elif level == 2:  # Return in recursion level is 2
+                print("111", changed)
+                return False
+            print("recursion")
+            two_lines("X", level, changed)  # Recursive check for "X"
 
         """Check if "O" wins now"""
         if type(o_move) == int:  # Check if there is a computer win
@@ -208,10 +214,14 @@ class Response:
         elif state.count(" ") == 2:  # Penultimate move
             state[state.index(" ")] = "O"
             print(6)
+            return
 
         else:
-            two_lines("O")
-            print(7)
+            two = two_lines("O", 0, False)
+            print(7777, two)
+            if two != True:
+                state[state.index(" ")] = "O"
+                print(8)
             return
 
     @staticmethod
